feac48fd9174dbf404aef2056fa2f51c1257cd40
David Anderson Ndaga <davidndaga1603@gmail.com>
2024-12-01 14:46:42 +0300

    Refactor chatbot and portal models: remove username field, add KnowledgeBaseDocument model, and update migrations

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Bot, BotTemplate
 
-# Register your models here.
+@admin.register(Bot)
+class BotAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    search_fields = ("name", "description")
+    list_filter = ("created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+
+
+@admin.register(BotTemplate)
+class BotTemplateAdmin(admin.ModelAdmin):
+    list_display = ("bot", "created_at", "updated_at")
+    search_fields = ("bot",)
+    list_filter = ("created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
